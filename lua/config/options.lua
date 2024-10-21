2fe59df1508547e54b5ed40832d596d5e779cf46
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-21 14:37:03 +0200

    feat(config): common options

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.autoindent = true
+vim.opt.copyindent = true
+vim.opt.preserveindent = true
+vim.opt.smartindent = true
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+vim.opt.hlsearch = false
+vim.opt.clipboard = "unnamedplus"
