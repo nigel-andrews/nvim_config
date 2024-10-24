5e7fd3a0ffaa79eb7caee3cbb2cdf99ee3d6f35c
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-24 10:13:48 +0200

    feat: add mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,11 +1,21 @@
 local map = vim.keymap.set
 
+-- Misc
 map('v', '<leader>/', 'gc', { desc = "Comment selection", remap = true });
+map('n', '<leader>c', '<cmd>bdelete<cr>', { desc = "Close current buffer"} );
+
+-- Buffer related
+map('n', '<PageUp>', '<cmd>bprevious<cr>');
+map('n', '<PageDown>', '<cmd>bnext<cr>');
+
+-- LSP related
 map('n', '<leader>la', function() vim.lsp.buf.code_action() end, { desc = 'LSP code action' })
 map('n', '<leader>ld', function() vim.diagnostic.open_float() end, { desc = 'Hover diagnostics' })
 map('n', '<leader>lr', function() vim.lsp.buf.rename() end, { desc = 'Rename symbol' })
 map('n', '<leader>lh', function() vim.lsp.buf.signature_help() end, { desc = 'Signature help' })
 map('n', '<leader>lG', function() vim.lsp.buf.workspace_symbol() end, { desc = 'Workspace symbol' })
+
+-- ToggleTerm
 map('n', '<leader>tt',  "<cmd>ToggleTerm<cr>", { desc = 'Open terminal' })
 map('n', '<leader>tf',  "<cmd>ToggleTerm direction=float<cr>", { desc = 'Open floatting terminal' })
 map('n', '<leader>tv',  "<cmd>ToggleTerm direction=vertical<cr>", { desc = 'Open terminal v-split' })
