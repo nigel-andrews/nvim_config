582b3630e611f1e23a5586f54e26cb14dfa0beaf
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-25 12:06:48 +0200

    feat: options: tabs for makefiles

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,8 @@ vim.opt.copyindent = true
 vim.opt.preserveindent = true
 vim.opt.smartindent = true
 
+vim.opt.completeopt = { "longest", "menuone" }
+
 vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -15,3 +17,17 @@ vim.opt.smarttab = true
 vim.opt.hlsearch = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.undofile = true
+
+vim.filetype.add({
+    extension = {
+        h = "c",
+    },
+})
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "»·", trail = "·" }
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "make" },
+    command = "setlocal noexpandtab shiftwidth=8",
+})
